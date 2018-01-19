13bd49e9e5dcb9a29dc4dda61782ae88b8589f19
tara kniep <tara.kniep@gmail.com>
2018-01-19 19:34:49 +0000

    Done.

---
+++ arrays.js
@@ -0,0 +1,7 @@
+
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(array, element){
+  chocolateBars = ["mr. goodbar", ...chocolateBars]
+}
+
