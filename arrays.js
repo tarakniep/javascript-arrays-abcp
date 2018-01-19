c1b66280dc02084053efea869ab7b78a44ce359b
tara kniep <tara.kniep@gmail.com>
2018-01-19 20:11:15 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -1,7 +1,43 @@
-
-var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element){
-  chocolateBars = ["mr. goodbar", ...chocolateBars]
+  var cities = ["1"];
+  ["foo",...cities];
+  return cities;
 }
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+
+}
+
+function addElementToEndOfArray(array, element){
+  
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  
+}
+
+function accessElementInArray(array, index){
+  
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  
+}
+
+function removeElementFromBeginningOfArray(array){
+  
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  
+}
+
+function removeElementFromEndOfArray(array){
+ 
+}
+
+
+
 
